b759a5703aaa649c8e128354ebc8b96ad62127bb
silentrald <silentrald1216@gmail.com>
2025-01-12 00:35:07 +0800

    Adjusted arrow keys to line up with hjkl

--- keebio/iris-rev8/keymap.c
+++ keebio/iris-rev8/keymap.c
@@ -19,10 +19,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    KC_LGUI, KC_PGDN, KC_PGUP, KC_HOME, KC_END , KC_CAPS , /*                  */ KC_INS  , KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_DEL ,
+    KC_LGUI, KC_PGDN, KC_PGUP, KC_HOME, KC_END , QK_GESC , /*                  */ KC_LEFT , KC_DOWN, KC_UP  , KC_RGHT, KC_INS , KC_DEL ,
     KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T    , /*                  */ KC_Y    , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
-    KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   , KC_G    , /*                  */ KC_H    , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
-    KC_LALT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B    , QK_GESC, /**/ QK_GESC, KC_N    , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+    KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   , KC_G    , /*                  */ KC_H    , KC_J 
+    KC_LALT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B    , KC_CAPS, /**/ QK_GESC, KC_N    , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
                                         KC_LCTL, MO(_SYM), KC_ENT , /**/ KC_SPC , MO(_MSC), KC_RCTL
   ),
   [_SYM] = LAYOUT(
