1f43e21c3fff5065f6629d37c18a4581efa494d5
silentrald <silentrald1216@gmail.com>
2025-01-12 16:26:35 +0800

    Adjusted the modifier and move layer keys

--- keebio/iris-rev8/keymap.c
+++ keebio/iris-rev8/keymap.c
@@ -6,8 +6,10 @@
 #define _BASE 0
 // Symbols layer
 #define _SYM 1
+// Number layer
+#define _NUM 2
 // Miscellaneous Management
-#define _MSC 2
+#define _MSC 3
 
 // LAYOUT(
 //   _______, _______, _______, _______, _______, _______, /*                  */ _______, _______, _______, _______, _______, _______,
@@ -19,17 +21,24 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    KC_LGUI, KC_PGDN, KC_PGUP, KC_HOME, KC_END , QK_GESC , /*                  */ KC_LEFT , KC_DOWN, KC_UP  , KC_RGHT, KC_INS , KC_DEL ,
-    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T    , /*                  */ KC_Y    , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
-    KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   , KC_G    , /*                  */ KC_H    , KC_J 
-    KC_LALT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B    , KC_CAPS, /**/ QK_GESC, KC_N    , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                                        KC_LCTL, MO(_SYM), KC_ENT , /**/ KC_SPC , MO(_MSC), KC_RCTL
+    KC_LGUI, KC_PGDN, KC_PGUP, KC_HOME, KC_END , QK_GESC, /*                  */ KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_INS , KC_DEL ,
+    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , /*                  */ KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
+    KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , /*                  */ KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
+   MO(_NUM), KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_LCTL, /**/ KC_CAPS, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+                                        KC_LALT,MO(_SYM), KC_ENT , /**/ KC_SPC , KC_RCTL, MO(_MSC)
   ),
   [_SYM] = LAYOUT(
-    _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , /*                  */ KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______,
-    _______, KC_GRV , KC_LT  , KC_GT  , KC_QUOT, KC_DQT , /*                  */ KC_AMPR, KC_COLN, KC_LBRC, KC_RBRC, KC_PERC, _______,
-    _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_HASH, /*                  */ KC_PIPE, KC_UNDS, KC_LPRN, KC_RPRN, KC_QUES, _______,
-    _______, KC_CIRC, KC_SLSH, KC_ASTR, KC_BSLS, KC_DOT , _______, /**/ _______, KC_TILD, KC_DLR , KC_LCBR, KC_RCBR, KC_AT  , _______,
+    _______, _______, _______, _______, _______, _______, /*                  */ _______, _______, _______, _______, _______, _______,
+    _______, KC_GRV , KC_AT  , KC_LBRC, KC_RBRC, KC_ASTR, /*                  */ KC_BSLS, KC_AMPR, KC_LCBR, KC_RCBR, KC_PERC, _______,
+    _______, KC_EXLM, KC_MINS, KC_PLUS, KC_EQL , KC_HASH, /*                  */ KC_PIPE, KC_UNDS, KC_LPRN, KC_RPRN, KC_COLN, KC_DQT  ,
+    _______, _______, _______, _______, KC_CIRC, KC_DOT , _______, /**/ _______, KC_TILD, KC_DLR , KC_LT  , KC_GT  , KC_QUES, _______,
+                                        _______, _______, _______, /**/ _______, _______, _______
+  ),
+  [_NUM] = LAYOUT(
+    _______, _______, _______, _______, _______, _______, /*                  */ _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, /*                  */ _______, KC_4   , KC_5   , KC_6   , _______, _______,
+    _______, _______, _______, _______, _______, _______, /*                  */ KC_0   , KC_1   , KC_2   , KC_3   , _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, /**/ _______, _______, KC_7   , KC_8   , KC_9   , _______, _______,
                                         _______, _______, _______, /**/ _______, _______, _______
   ),
   [_MSC] = LAYOUT(
